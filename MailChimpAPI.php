4600ba56b2e87e6855e19c0ab6fd81b4f7fdff11
Isaac Grant <isaac@silverorange.com>
2011-11-07 12:13:31 +0000

    rename the class name as well as the file to match conventions.
    svn commit r66487

--- MailChimpAPI.php
+++ MailChimpAPI.php
@@ -36,7 +36,7 @@ class MailChimpAPI {
      * @param string $apikey Your MailChimp apikey
      * @param string $secure Whether or not this should use a secure connection
      */
-    function MCAPI($apikey, $secure=false) {
+    function MailChimpAPI($apikey, $secure=false) {
         $this->secure = $secure;
         $this->apiUrl = parse_url("http://api.mailchimp.com/" . $this->version . "/?output=php");
         $this->api_key = $apikey;
